7ee7312e186b0bb79302aefc69255f14f998b375
BradGrensewich <bradgrensewich@gmail.com>
2025-01-28 11:30:31 +0700

    exercise 3.16

--- index.js
+++ index.js
@@ -31,10 +31,12 @@ app.use(
 		].join(' ');
 	})
 );
-app.get('/api/persons', (req, res) => {
-	Person.find({}).then((persons) => {
-		return res.status(200).json(persons);
-	});
+app.get('/api/persons', (req, res, next) => {
+	Person.find({})
+		.then((persons) => {
+			return res.status(200).json(persons);
+		})
+		.catch((error) => next(error));
 });
 app.get('/api/persons/:id', (req, res) => {
 	const id = req.params.id;
@@ -44,16 +46,15 @@ app.get('/api/persons/:id', (req, res) => {
 	}
 	return res.status(200).json(person);
 });
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
 	const id = req.params.id;
-	Person.findByIdAndDelete(id).then(result => {
-		res.status(204).end()
-	}).catch(error => {
-		console.error(error.message)
-		res.status(500).json({error: error.message})
-	})
+	Person.findByIdAndDelete(id)
+		.then((result) => {
+			res.status(204).end();
+		})
+		.catch((error) => next(error));
 });
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
 	const body = req.body;
 	if (!body.name || !body.number) {
 		return res.status(400).json({
@@ -64,9 +65,12 @@ app.post('/api/persons', (req, res) => {
 		name: body.name,
 		number: body.number,
 	});
-	person.save(person).then((result) => {
-		res.status(201).json(result);
-	});
+	person
+		.save(person)
+		.then((result) => {
+			res.status(201).json(result);
+		})
+		.catch((error) => next(error));
 });
 
 app.get('/api/info', (req, res) => {
@@ -77,6 +81,16 @@ app.get('/api/info', (req, res) => {
         <p>${time}</p>`;
 	res.status(200).send(info);
 });
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message);
+	if (error.name === 'CastError') {
+		return res.status(400).send({ error: 'malformed id' });
+	}
+	next(error);
+};
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
